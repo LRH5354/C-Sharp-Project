3c827574593619ee2f74793a098d984de72c7a3c
LRH <1587910290@qq.com>
2021-01-04 15:31:45 +0800

    the first commit

---
+++ 控制台程序/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 控制台程序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
